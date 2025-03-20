6908245bb8bcee23eb6931999fe7222b589fd427
okyunsu <okyunsu@gmail.com>
2025-03-20 10:50:11 +0900

    25.03.20

--- src/app/account/guest/templates/create_guest/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useSignup } from '@/hooks/auth/useSignup';
-
-export default function CreateGuestPage() {
-  const { formData, handleChange, errors, handleSubmit } = useSignup();
-  
-  return (
-    // ... 컴포넌트 내용
-  );
-} 
