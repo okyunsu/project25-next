2cbe7a5559d8a39188702da845d01343f3681ab7
okyunsu <okyunsu@gmail.com>
2025-03-25 15:33:13 +0900

    25.03.25

--- okyunsu/next.config.js
+++ okyunsu/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig; 
--- okyunsu/src/app/(account)/auth/login/page.tsx
+++ okyunsu/src/app/(account)/auth/login/page.tsx
@@ -2,6 +2,8 @@
 
 import LoginForm from '@/components/(account)/auth/LoginForm';
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
     return <LoginForm />;
 }  
